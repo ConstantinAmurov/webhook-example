e8fe2ab80449181689525d5b103a6bcdc3909ef0
Stefan Tcaciuc <stefan.tcaciuc@assist.ro>
2021-12-21 14:13:06 +0200

    [SON-450] Start-up

---
+++ scripts/utils/config.js
@@ -0,0 +1,36 @@
+const bbCore = require('../sdk');
+
+const CONFIG_PROPERTIES = [
+    'configJson'
+];
+
+const getConfItem = name => {
+    if (!CONFIG_PROPERTIES.includes(name))
+        return null;
+
+    const configValue = bbCore.getConfigValue(name);
+    return configValue === null ? '' : configValue;
+};
+
+const getConfItems = names => {
+    return names.reduce((items, name) => {
+        items[name] = getConfItem(name);
+        return items;
+    }, {});
+};
+
+const getAllConfItems = () => {
+    return getConfItems(CONFIG_PROPERTIES);
+};
+
+const setConfItem = (name, value) => {
+    return bbCore.setConfigValue(name, value);
+};
+
+module.exports = {
+    CONFIG_PROPERTIES,
+    getConfItem,
+    getConfItems,
+    getAllConfItems,
+    setConfItem
+};
