d00e543c964654d9554acbcabe3d57cf103ec2e4
CostelMD <amurov.constantin@gmail.com>
2022-02-10 15:11:16 +0100

    [SON-479] Updated  previous version of requesting liquid items to the new one using liquid template

--- scripts/utils/liquid-payload-parser.js
+++ scripts/utils/liquid-payload-parser.js
@@ -1,119 +1,29 @@
 const bbCore = require('../sdk');
 
-const getGroupedLiquidItems = (payload) => {
-    const liquidItemRegex = /\{\{[^{}]+\}\}/g; // {{person.name}}
-    const liquidItems = payload.match(liquidItemRegex);
-
-    if (liquidItems === null) return {};
-
-    const existingEntities = [
-        'booking',
-        'company',
-        // 'address',
-        'person',
-        'resource',
-        'service',
-        'client',
-        'slot',
-        // 'total',
-        // 'item',
-        // 'app',
-        // 'object',
-        // 'attendance',
-        // 'attendee',
-        // 'extra_data',
-        // 'message',
-        'event_chain',
-        'user',
-        // 'recipient',
-        // 'category',
-    ];
-
-    const entityRegex = new RegExp(`(${existingEntities.join('|')})(?=\\.)`);
-    const liquidGroups = {};
-
-    liquidItems.forEach(liquidItem => {
-        const entities = liquidItem.match(entityRegex);
-        const entity = entities ? entities[0] : 'noEntity';
-        if (!liquidGroups.hasOwnProperty(entity))
-            liquidGroups[entity] = {};
-
-        const key = liquidItem.replace(/[{}]/g, '');
-        liquidGroups[entity][key] = liquidItem;
-    });
-
-    if (liquidGroups.hasOwnProperty('noEntity')) {
-        const entities = Object.keys(liquidGroups);
-        if (entities.length === 1) {
-            liquidGroups.booking = liquidGroups.noEntity;
-        }
-        else {
-            const randomEntity = entities.filter(entity => entity !== 'noEntity')[0];
-            liquidGroups[randomEntity] = { ...liquidGroups[randomEntity], ...liquidGroups.noEntity };
-        }
-        delete liquidGroups.noEntity;
-    }
-
-    return liquidGroups;
-};
-
-const getEntityId = (entity, booking) => {
-    switch (entity) {
-        case 'booking':
-            return booking.id;
-
-        default:
-            return booking[`${entity}_id`];
-    }
-};
-
-const getResolvedLiquidItems = async (groupedLiquidItems, booking) => {
+const getResolvedLiquidItems = async (payloads, booking) => {
     try {
         const company = await bbCore.getCompany();
         const requests = [];
 
-        for (const [entity, liquidItemsGroup] of Object.entries(groupedLiquidItems)) {
-            const entityId = getEntityId(entity, booking);
-
+        payloads.forEach((payload) => {
             const params = {
-                entity,
-                id: entityId,
+                entity: 'booking',
+                id: booking.id,
             };
             const data = {
                 version: 'V4',
-                liquid_template: JSON.stringify(liquidItemsGroup)
+                liquid_template: payload
             };
             requests.push(company.$post('liquid_renderer', params, data));
-        }
+        });
 
-        const results = await Promise.all(requests);
-        return results.reduce((resolvedLiquidItems, result) => ({ ...resolvedLiquidItems, ...JSON.parse(result.liquid_render) }), {});
+        let results = await Promise.all(requests);
+
+        results = results.map((result) => JSON.parse(result.liquid_render));
+
+        return results;
     } catch (error) {
         error.source = error.source || 'liquid-payload-parser.js -> getResolvedLiquidItems';
-        throw error;
-    }
-};
-
-const getLiquidResolvedPayload = async (payload, booking) => {
-    try {
-        payload = JSON.stringify(payload);
-
-        // Remove spaces next to tags
-        payload = payload.replace(/(?<=\{\{)\s*/g, ''); // {{   person.name   }} -> {{person.name   }}
-        payload = payload.replace(/\s*(?=\}\})/g, '');  // {{person.name   }} -> {{person.name}}
-
-        const groupedLiquidItems = getGroupedLiquidItems(payload);
-        const resolvedLiquidItems = await getResolvedLiquidItems(groupedLiquidItems, booking);
-
-        for (let [liquidItem, jrniValue] of Object.entries(resolvedLiquidItems)) {
-            liquidItem = '{{' + liquidItem + '}}';
-            payload = payload.split(liquidItem).join(jrniValue);
-        }
-
-        payload = JSON.parse(payload);
-        return payload;
-    } catch (error) {
-        error.source = error.source || 'liquid-payload-parser.js -> getLiquidResolvedPayload';
         throw error;
     }
 };
@@ -121,29 +31,15 @@ const getLiquidResolvedPayload = async (payload, booking) => {
 const getLiquidResolvedPayloads = async (payloads, booking) => {
     try {
         payloads = payloads.map(payload => {
-            let jsonPayload = JSON.stringify(payload);
-
             // Remove spaces next to tags
-            jsonPayload = jsonPayload.replace(/(?<=\{\{)\s*/g, ''); // {{   person.name   }} -> {{person.name   }}
-            jsonPayload = jsonPayload.replace(/\s*(?=\}\})/g, '');  // {{person.name   }} -> {{person.name}}
-
-            return jsonPayload;
+            payload = payload.replace(/(?<=\{\{)\s*/g, ''); // {{   person.name   }} -> {{person.name   }}
+            payload = payload.replace(/\s*(?=\}\})/g, '');  // {{person.name   }} -> {{person.name}}
+            return payload;
         });
 
-        let joinedPayloads = payloads.join('');
+        const resolvedLiquidItems = await getResolvedLiquidItems(payloads, booking);
 
-        const groupedLiquidItems = getGroupedLiquidItems(joinedPayloads);
-        const resolvedLiquidItems = await getResolvedLiquidItems(groupedLiquidItems, booking);
-
-        for (let [liquidItem, jrniValue] of Object.entries(resolvedLiquidItems)) {
-            liquidItem = '{{' + liquidItem + '}}';
-            payloads.forEach((payload, index) => {
-                payloads[index] = payload.split(liquidItem).join(jrniValue);
-            });
-        }
-
-        payloads = payloads.map(payload => JSON.parse(payload));
-        return payloads;
+        return resolvedLiquidItems;
     } catch (error) {
         error.source = error.source || 'liquid-payload-parser.js -> getLiquidResolvedPayloads';
         throw error;
@@ -151,6 +47,5 @@ const getLiquidResolvedPayloads = async (payloads, booking) => {
 };
 
 module.exports = {
-    getLiquidResolvedPayload,
     getLiquidResolvedPayloads
 };
