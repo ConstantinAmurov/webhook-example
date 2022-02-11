1b63eafb779f8c4c4c81e50b782aaabc9d06c288
Stefan Tcaciuc <stefan.tcaciuc@assist.ro>
2022-02-11 19:39:42 +0200

    [SON-486] Improve the UI by using a form instead of JSON for config

--- scripts/config.js
+++ scripts/config.js
@@ -4,42 +4,15 @@ const { getConfItem, setConfItem } = require('./utils/config');
 const log = require('./utils/logger');
 const configSchema = require('./config-schema');
 
-const getWebhookConfig = (data, callback) => {
-    try {
-        const configJson = getConfItem('configJson') || '[]';
-        const config = JSON.parse(configJson);
-        parseConfigBeforeSending(config);
-        callback(null, { config });
-    } catch (error) {
-        error.source = error.source || 'config.js -> getWebhookConfig';
-        log('error', `[${error.source}]`, error, true);
-        callback(new Error(`Failed to retrieve app config. Error: ${error.message}.`));
-    }
-};
-
-const validateConfig = config => {
-    const { errors } = jsonSchemaValidate(config, configSchema);
-
-    if (errors.length > 0) {
-        const err = new Error(errors[0].stack);
-        err.source = 'config.js -> validateConfig';
-        throw err;
-    }
-};
-
-const saveWebhookConfig = (data, callback) => {
-    try {
-        const { config } = data;
-        parseReceivedConfig(config);
-        validateConfig(config);
-        setConfItem('configJson', JSON.stringify(config));
-        parseConfigBeforeSending(config);
-        callback(null, { config });
-    } catch (error) {
-        error.source = error.source || 'config.js -> saveWebhookConfig';
-        log('error', `[${error.source}]`, error, true);
-        callback(new Error(`Failed to save app config. Error: ${error.message}.`));
-    }
+const setWebHookConfigDefaultValues = (config) => {
+    config.forEach((item) => {
+        item.events = item.events || [];
+        item.triggerFor = item.triggerFor || {};
+        item.triggerFor.companies = item.triggerFor.companies || [];
+        item.triggerFor.staffGroups = item.triggerFor.staffGroups || [];
+        item.triggerFor.parentCompanies = item.triggerFor.parentCompanies || [];
+        item.triggerFor.excludedCompanies = item.triggerFor.excludedCompanies || [];
+    });
 };
 
 const parseReceivedConfig = (config) => {
@@ -62,15 +35,42 @@ const parseConfigBeforeSending = (config) => {
     });
 };
 
-const setWebHookConfigDefaultValues = (config) => {
-    config.forEach((item) => {
-        item.events = item.events || [];
-        item.triggerFor = item.triggerFor || {};
-        item.triggerFor.companies = item.triggerFor.companies || [];
-        item.triggerFor.staffGroups = item.triggerFor.staffGroups || [];
-        item.triggerFor.parentCompanies = item.triggerFor.parentCompanies || [];
-        item.triggerFor.excludedCompanies = item.triggerFor.excludedCompanies || [];
-    });
+const validateConfig = config => {
+    const { errors } = jsonSchemaValidate(config, configSchema);
+
+    if (errors.length > 0) {
+        const err = new Error(errors[0].stack);
+        err.source = 'config.js -> validateConfig';
+        throw err;
+    }
+};
+
+const getWebhookConfig = (data, callback) => {
+    try {
+        const configJson = getConfItem('configJson') || '[]';
+        const config = JSON.parse(configJson);
+        parseConfigBeforeSending(config);
+        callback(null, { config });
+    } catch (error) {
+        error.source = error.source || 'config.js -> getWebhookConfig';
+        log('error', `[${error.source}]`, error, true);
+        callback(new Error(`Failed to retrieve app config. Error: ${error.message}.`));
+    }
+};
+
+const saveWebhookConfig = (data, callback) => {
+    try {
+        const { config } = data;
+        parseReceivedConfig(config);
+        validateConfig(config);
+        setConfItem('configJson', JSON.stringify(config));
+        parseConfigBeforeSending(config);
+        callback(null, { config });
+    } catch (error) {
+        error.source = error.source || 'config.js -> saveWebhookConfig';
+        log('error', `[${error.source}]`, error, true);
+        callback(new Error(`Failed to save app config. Error: ${error.message}.`));
+    }
 };
 
 module.exports = {
